d85e581a6b02565e23ef94af5c97b8d17d5405c5
Emily Hamel <em.jhamel@gmail.com>
2021-05-01 16:58:09 -0400

    figures 1 - 3

--- shorebird_project.R
+++ shorebird_project.R
@@ -12,6 +12,7 @@ disturbance_README <- read_csv("DATA/README_for_Immed.displ.analysis.csv")
 #Figure 1 plot
 ggplot(disturbance, aes(movement_category, distance)) + 
   geom_point() + 
+  theme_classic() +
   labs(x = "Movement Category", 
                         y = "Immediate Displacement Distance (km)", 
                         title = "Immediate Displacement as a Result of Disturbance",
@@ -19,5 +20,23 @@ ggplot(disturbance, aes(movement_category, distance)) +
                         categories. The points are indicative of each displacement event of the 240 observations.")
 #Short-term disturbance statistical test
 model_1 <- kruskal.test(distance ~ movement_category, data = disturbance) #overall significance 
-pairwise.wilcox.test(disturbance$distance, g = disturbance$movement_category) #pairwise significance across categories
+model_1_pair <- pairwise.wilcox.test(disturbance$distance, g = disturbance$movement_category) #pairwise significance across categories
 
+
+#Figure 2 plot
+ggplot(hightide, aes(waterlevel, distance_total, color = roost)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  theme_classic() +
+  labs(x = "Maximum Water Level at High Tide (m)", 
+       y = "Distance Traveled in High Tide Interval (km)", 
+       color = "Roost Site", 
+       title = "High Tide Travel Distance and Water Level",
+       caption = "Figure 2. Relationship between the distance birds traveled during intervals of high tide and the maximum water level during these events.
+       Data is divided between three relevant roosting sites: Mid, North, and South (shown in red, green, and blue - respectively).") 
+
+#Figure 3 plot
+ggplot(hightide, aes(distance_disturbed, distance_total)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  theme_classic()
